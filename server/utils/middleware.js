6f4402f7988d8543c36d96e91cf1e9573a80dea5
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-05-11 20:10:18 +0300

    server side jwt user verification middleware

---
+++ server/utils/middleware.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const ExpressError = require("../utils/ExpressError");
+
+//JWT VERIFICATION
+const verifyUser = (req, res, next) => {
+  const userJwtHeader = req.headers.token;
+  if (!userJwtHeader)
+    throw new ExpressError("You Are not authenticated to Update", 401);
+  const token = userJwtHeader.split(" ")[1];
+  jwt.verify(token, process.env.JWT_KEY, (err, user) => {
+    if (err) throw new ExpressError("Token not valid", 403);
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = verifyUser;
