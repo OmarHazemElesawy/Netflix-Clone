420bb7e2ab11112a44bc4e160889135a42ecc357
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-05-11 23:05:04 +0300

    added Context store for client side

---
+++ client/src/store/authContext/apiCalls.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
+export const login = async (user, dispatch) => {
+  const loginUrl =  process.env.REACT_APP_LOGIN_URL;
+  dispatch(loginStart());
+  try {
+    console.log("loginUrl",loginUrl)
+    const res = await axios.post(loginUrl, user);
+    dispatch(loginSuccess(res.data));
+  } catch {
+    dispatch(loginFailure("wrong email or password"));
+  }
+};
+
