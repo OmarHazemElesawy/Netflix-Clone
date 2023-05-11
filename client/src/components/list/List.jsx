edd559086da502b5b54564ea3f11cedd2416e182
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-05-11 23:09:57 +0300

    added List component to client

---
+++ client/src/components/list/List.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useState } from "react";
+import "./list.scss";
+import {
+  ArrowBackIosNewOutlined,
+  ArrowForwardIosOutlined,
+} from "@mui/icons-material";
+import Item from "../item/Item";
+const List = ({list}) => {
+  list.content = list.content.sort((a, b) => 0.5 - Math.random());
+  const [itemNumber, setItemNumber] = useState(0);
+  const [isClicked, setIsClicked] = useState(false);
+  const listRef = useRef();
+
+  const handleClick = (direction) => {
+    setIsClicked(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction === "left" && itemNumber > 0) {
+      setItemNumber((previousState) => {
+        return previousState - 1;
+      });
+      listRef.current.style.transform = `translate(${255 + distance}px)`;
+    }
+    if (direction === "right" && itemNumber < 5) {
+      setItemNumber((previousState) => {
+        return previousState + 1;
+      });
+      listRef.current.style.transform = `translate(${-255 + distance}px)`;
+    }
+  };
+  return (
+    <div className="list">
+      <span className="listTitle">{list.title}</span>
+      <div className="container">
+        <ArrowBackIosNewOutlined
+          className="arrow left"
+          onClick={() => {
+            handleClick("left");
+          }}
+          style={{ display: !isClicked && "none" }}
+        />
+        <div ref={listRef} className="innerContainer">
+          {list.content.map((movie, i) => (
+            <Item key={movie._id} index={i} item={movie}/>
+          ))}
+        </div>
+        <ArrowForwardIosOutlined
+          className="arrow right"
+          onClick={() => {
+            handleClick("right");
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default List;
