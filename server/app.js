9cc5c4150e4688856e78fdc257efb8c7200eef20
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-05-12 00:27:36 +0300

    updated server side app file

--- server/app.js
+++ server/app.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 //routes import
 const authRoutes = require("./routes/auth");
