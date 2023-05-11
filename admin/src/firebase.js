742d9190db5b91fe43608dd1f8f5e867a3bf2dca
OmarHazemElesawy <omarhazem2204@gmail.com>
2023-05-12 00:18:25 +0300

    updated client side package file

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3EFLT-Rsp7rBkwGxvt9_ovnSPHEZYDic",
-  authDomain: "netflix-a03c3.firebaseapp.com",
-  projectId: "netflix-a03c3",
-  storageBucket: "netflix-a03c3.appspot.com",
-  messagingSenderId: "209419573781",
-  appId: "1:209419573781:web:451cec0f726b7ab170cc1c",
-  measurementId: "G-M1MBQ6SR2S",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
